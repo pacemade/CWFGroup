110b120e15120776996ffed108e461e410ad7001
Danny Chang <dannychachang@gmail.com>
2018-05-01 12:31:32 -0400

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
    sessions: 'users/sessions'
  }
 
+ resources :users, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
