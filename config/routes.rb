cfb01d9de5caa12828752fa68d09db20acce3633
Danny Chang <dannychachang@gmail.com>
2018-05-01 12:13:48 -0400

    using custom devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'home/index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+   sessions: 'users/sessions'
+ }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
