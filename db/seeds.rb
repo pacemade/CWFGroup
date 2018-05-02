cbd868d65c7fc06bcf0ddfc3cd2006e17b0ca9b8
Danny Chang <dannychachang@gmail.com>
2018-05-02 15:20:24 -0400

    add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "admin", last_name: "admin", email: "admin@admin.com", admin: true, password: "password", password_confirmation: "password")
