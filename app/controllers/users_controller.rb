fe135ac2cce39d82dd6fa7e41c2b30c71cd414a0
Danny Chang <dannychachang@gmail.com>
2018-05-02 15:30:36 -0400

    add view links for users index, validations for first/last name fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
-
-
-
-private
+  private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
