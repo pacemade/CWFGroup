73eff5db073cedc28b350b717713e90dfa7d85ae
Danny Chang <dannychachang@gmail.com>
2018-05-04 12:15:23 -0400

    basic javascript working on new trips page

---
+++ app/assets/javascripts/results.js
@@ -0,0 +1,10 @@
+document.addEventListener("turbolinks:load", function() {
+
+  if (document.getElementById('see_options')) {
+
+    console.log("i see you");
+    const buttonEle = document.getElementById('see_options');
+    console.log(buttonEle);
+
+  }
+})
