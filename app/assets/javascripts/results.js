3209c02f469421f8bb6762680f1ad1199dcc61d7
Danny Chang <dannychachang@gmail.com>
2018-05-07 17:13:56 -0400

    revert js for now

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -4,9 +4,10 @@ document.addEventListener("turbolinks:load", function() {
 
     console.log("i see you");
     var seeOptions = document.getElementById('see_options');
+    console.log(seeOptions);
 
     seeOptions.addEventListener('submit', function(e) {
-      e.preventDefault();
+
     });
 
   }
