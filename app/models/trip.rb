5c3c142c1e7eef3d0350fd8b88d129026e414676
Danny Chang <dannychachang@gmail.com>
2018-05-07 16:45:09 -0400

    add validation for start and end dates, error message does not include key

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,14 @@ class Trip < ApplicationRecord
 
   belongs_to :user
 
-  validates :first_name, :last_name, :start_date, :end_date, :birthday, presence: true
+  validates :first_name, :last_name, :start_date, :end_date, :birthday, :coverage, presence: true
+  validate :end_time_cannot_be_before_start
+
+
+  def end_time_cannot_be_before_start
+    if self[:start_date] > self[:end_date]
+      errors.add(:_, "The end date cannot be before the start date. Please try again.")
+    end
+  end
 
 end
