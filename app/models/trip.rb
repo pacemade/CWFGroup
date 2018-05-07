b300515c0519dec8443d2d1f9cd99c968414a182
Danny Chang <dannychachang@gmail.com>
2018-05-07 16:49:57 -0400

    add birthday calculator

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,6 +6,11 @@ class Trip < ApplicationRecord
   validate :end_time_cannot_be_before_start
 
 
+  def age(birthday)
+    now = Time.now.utc.to_date
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
   def end_time_cannot_be_before_start
     if self[:start_date] > self[:end_date]
       errors.add(:_, "The end date cannot be before the start date. Please try again.")
