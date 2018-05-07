af9ccbaa409a2968221137588aa585e2430626ad
Danny Chang <dannychachang@gmail.com>
2018-05-07 17:29:56 -0400

    create temp controller for policies, prevent default finally works

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -3,11 +3,11 @@ document.addEventListener("turbolinks:load", function() {
   if (document.getElementById('see_options')) {
 
     console.log("i see you");
-    var seeOptions = document.getElementById('see_options');
-    console.log(seeOptions);
+    var seeOptions = document.querySelector('#see_options');
 
-    seeOptions.addEventListener('submit', function(e) {
-
+    seeOptions.addEventListener('click', function(e) {
+      e.preventDefault();
+      console.log("is this working");
     });
 
   }
---
+++ app/controllers/policies_controller.rb
@@ -0,0 +1,6 @@
+class PoliciesController < ApplicationController
+
+  def index
+  end
+
+end
