5ac0008b2dc0e1440853404fcde1aa1e6c0479fa
Danny Chang <dannychachang@gmail.com>
2018-05-08 10:52:02 -0400

    age calculator works, results work with age less than 100

--- app/controllers/concerns/search_policies.rb
+++ app/controllers/concerns/search_policies.rb
@@ -1,15 +1,25 @@
 class SearchPolicies
 
+  attr_reader :results
+
   def initialize(params)
     @params = params
+    convert_birthday
+    @age = age(@birthday)
+    @results = Policy.where('min_age <= ? and max_age >= ?', @age, @age)
   end
 
   def filter_by_coverage
   end
 
+  def convert_birthday
+    trip = @params[:trip]
+    @birthday = Date.new trip["birthday(1i)"].to_i, trip["birthday(2i)"].to_i, trip["birthday(3i)"].to_i
+  end
+
   def age(birthday)
     now = Time.now.utc.to_date
-    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+    now.year - @birthday.year - ((now.month > @birthday.month || (now.month == @birthday.month && now.day >= @birthday.day)) ? 0 : 1)
   end
 
 end
