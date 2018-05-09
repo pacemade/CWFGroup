8d25116ee81c80e0d0930801bfeb254052125711
Danny Chang <dannychachang@gmail.com>
2018-05-09 11:29:04 -0400

    create helper method in trip model to determine age

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,8 @@
 module TripsHelper
+
+  def find_age(birthday)
+    now = Time.now.utc.to_date
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+  
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,12 +6,6 @@ class Trip < ApplicationRecord
   validates :first_name, :last_name, :start_date, :end_date, :birthday, :coverage, presence: true
   validate :end_time_cannot_be_before_start
 
-
-  def age(birthday)
-    now = Time.now.utc.to_date
-    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
-  end
-
   def end_time_cannot_be_before_start
     if self[:start_date] && self[:end_date] && self[:start_date] > self[:end_date]
       errors.add(:_, "The end date cannot be before the start date. Please try again.")
